e2b38c4f3df733d0ee3167eb47f3194c561f8d01
Patricio Escalona <patricioescabe@gmail.com>
2025-04-12 13:16:39 +0200

    añadidos controllers para recuperación de password con envío de code por mail

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,10 +3,14 @@ import newUserController from './newUserController.js';
 import activateUserController from './activateUserController.js';
 import loginUserController from './loginUserController.js';
 import getPrivateUserInfoController from './getPrivateUserInfoController.js';
+import sendRecoverPassController from './sendRecoverPassController.js';
+import resetUserPassWithCodeController from './resetUserPassWithCodeController.js';
 
 export {
   newUserController,
   activateUserController,
   loginUserController,
   getPrivateUserInfoController,
+  sendRecoverPassController,
+  resetUserPassWithCodeController,
 };
