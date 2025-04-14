70cd0b7b795d9df7e3e142c92b1921377a3c010f
dpaezm <diegopaezmacias@gmail.com>
2025-04-14 15:59:59 +0200

    Actualizar rutas y datos de prueba en bbdd

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -76,16 +76,134 @@ try {
 
   await pool.query(
     `
-      INSERT INTO user (username, email, password, isadmin) 
-      VALUES (?, ?, ?, ?)
-      `,
-    ['admin1', 'admin1@proveedor-perfecto.com', hashedPassword, true],
+  INSERT INTO user (username, email, password, active, isadmin, isprovider)
+  VALUES ?
+  `,
+    [
+      [
+        [
+          'admin1',
+          'admin1@proveedor-perfecto.com',
+          hashedPassword,
+          true,
+          true,
+          false,
+        ],
+        [
+          'admin2',
+          'admin2@proveedor-perfecto.com',
+          hashedPassword,
+          true,
+          true,
+          false,
+        ],
+        [
+          'admin3',
+          'admin3@proveedor-perfecto.com',
+          hashedPassword,
+          true,
+          true,
+          false,
+        ],
+        [
+          'Provider1',
+          'provider1@provider.com',
+          hashedPassword,
+          true,
+          false,
+          true,
+        ],
+        [
+          'Provider2',
+          'provider2@provider.com',
+          hashedPassword,
+          true,
+          false,
+          true,
+        ],
+        [
+          'Provider3',
+          'provider3@provider.com',
+          hashedPassword,
+          true,
+          false,
+          true,
+        ],
+        ['Buyer1', 'buyer1@buyer.com', hashedPassword, true, false, false],
+        ['Buyer2', 'buyer2@buyer.com', hashedPassword, true, false, false],
+        ['Buyer3', 'buyer3@buyer.com', hashedPassword, true, false, false],
+      ],
+    ],
   );
 
   await pool.query(`
       INSERT INTO category (categoryname)
       VALUES ("Programming & Tech"), ("Graphics & Design"), ("Marketing"), ("Copywritting"), ("AI services"), ("Finance"), ("Bussines") ;
       `);
+
+  await pool.query(
+    `INSERT INTO product (owner_id, category_id, product_name, price, description) VALUES ?`,
+    [
+      [
+        [
+          4,
+          1,
+          'Desarrollo SaaS',
+          999,
+          'Desarrollaremos tu propio SaaS a medida',
+        ],
+        [
+          4,
+          7,
+          'Bussines plan',
+          650,
+          'Te ayudaremos a crear un plan de negocio',
+        ],
+        [
+          5,
+          3,
+          'Gestión de Social Media',
+          750,
+          'Contrátanos si necesitas un Community Manager',
+        ],
+        [5, 3, 'Social Media Ads', 450, 'Configuramos tus anuncios a medida'],
+        [6, 6, 'Dirección financiera', 1500, 'Te ayudo a encontrar tu CFO'],
+      ],
+    ],
+  );
+
+  await pool.query(
+    `
+  INSERT INTO contact (user_id, product_id, comment, status)
+  VALUES ?
+  `,
+    [
+      [
+        [7, 1, 'Quiero conocer más detalles sobre tu producto', 'inicio'],
+        [7, 3, 'Quiero conocer más detalles sobre tu producto', 'inicio'],
+        [8, 5, 'Voy a contratar tus servicios', 'tramitando'],
+      ],
+    ],
+  );
+
+  await pool.query(
+    `
+  INSERT INTO contact (user_id, product_id, comment, status, rating)
+  VALUES ?
+  `,
+    [
+      [
+        [
+          8,
+          1,
+          'Excelente profesional, atendió todas nuestras consultas',
+          'finalizado',
+          '5',
+        ],
+        [9, 5, 'Nos dejó colgados en el último momento', 'cancelado', '1'],
+      ],
+    ],
+  );
 
   console.log('Datos introducidos');
 
--- src/models/userModels/saveRecoverPassCodeModel.js
+++ src/models/userModels/saveRecoverPassCodeModel.js
@@ -1,4 +1,4 @@
-import getPool from '../db/getPool.js';
+import getPool from '../../db/getPool.js';
 
 const saveRecoverPassCodeModel = async (email, recoverPassCode) => {
   const pool = await getPool();
