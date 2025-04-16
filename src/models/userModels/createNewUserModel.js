00d4b78ac850fc20842131ff4d1124d794c83afa
Patricio Escalona <patricioescabe@gmail.com>
2025-04-17 00:21:38 +0200

    terminada la funcionalidad de activación de proveedores mediante envío de email

--- src/models/userModels/createNewUserModel.js
+++ src/models/userModels/createNewUserModel.js
@@ -4,14 +4,15 @@ const createNewUserModel = async ({
   username,
   email,
   hashedPass,
+  isProvider,
   registrationCode,
 }) => {
   const pool = await getPool();
 
   // Insertamos el usuario con el registrationCode incluido
   await pool.query(
-    `INSERT INTO user (username, email, password, registrationCode) VALUES (?, ?, ?, ?)`,
-    [username, email, hashedPass, registrationCode],
+    `INSERT INTO user (username, email, password, isProvider, registrationCode) VALUES (?, ?, ?, ?, ?)`,
+    [username, email, hashedPass, isProvider, registrationCode],
   );
 };
 
