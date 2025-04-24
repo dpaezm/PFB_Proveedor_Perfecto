b244c0ac7a36429d7c8481f9b25d7734ac5fe755
Andres Sanchez <flakotulay@hotmail.com>
2025-04-24 11:24:20 +0200

    resubido login y registro

--- FRONT-Proveedor-Perfecto/src/components/Nav.jsx
+++ FRONT-Proveedor-Perfecto/src/components/Nav.jsx
@@ -11,6 +11,9 @@ export default function Nav() {
           <NavLink to="/register">Registro</NavLink>
         </li>
         <li className="text-destacado md:text-[14px] text-amarillo font-medium text-center">
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li className="text-destacado md:text-[14px] text-amarillo font-medium text-center">
           <NavLink to="/muestra">Muestra</NavLink>
         </li>
       </menu>
