07b63ea1235517cf6e8890b925dd396435af63f3
Andres Sanchez <flakotulay@hotmail.com>
2025-04-27 22:42:53 +0200

    tailwind a formularios login y registro

--- FRONT-Proveedor-Perfecto/src/components/Nav2.jsx
+++
@@ -1,9 +0,0 @@
-import burgerIcon2 from "../assets/icons/menu_hamburger.png";
-
-export default function Nav2() {
-  return (
-    <nav className="nav2">
-      <button id="burger-icon" style={{ backgroundImage: `url(${burgerIcon2})` }}></button>
-    </nav>
-  );
-}
