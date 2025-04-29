fdd5ebb9ef89aecaf20dcfd2abac71c873f56486
Patricio Escalona <patricioescabe@gmail.com>
2025-04-29 17:13:23 +0200

    actualización del diseño del createPrduct

--- FRONT-Proveedor-Perfecto/src/pages/CreateProduct.jsx
+++ FRONT-Proveedor-Perfecto/src/pages/CreateProduct.jsx
@@ -8,20 +8,22 @@ export default function CreateProduct() {
     useCreateProduct();
 
   return (
-    <main className="p-5">
-      <h2 className="text-cta md:text-[36px] text-amarillo font-bold text-center">
+    <main className="mx-auto px-4 py-8 md:px-16 lg:px-32 ">
+      <h2 className="text-cta md:text-[36px] text-amarillo font-bold text-center mb-8">
         Crear Producto
       </h2>
 
-      {error && <p className="text-red-500 font-bold">{error}</p>}
+      {error && <p className="text-red-500 font-bold text-center">{error}</p>}
 
       <form
-        className="p-5 border-solid border-2 rounded-md text-normal text-gris1 font-normal text-center"
         onSubmit={handleSubmit}
+        className="w-full md:w-80% mx-auto flex flex-col gap-6 md:gap-8 items-center max-w-[900px] "
       >
-        <ul className="p-2 flex-col space-y-4">
-          <li className="p-2">
-            <label htmlFor="product_name">Nombre del producto:</label>
+        <div className="flex flex-col md:flex-row gap-4 w-full">
+          <div className="w-full md:w-1/2">
+            <label htmlFor="product_name" className="block mb-2">
+              Nombre del producto:
+            </label>
             <Input
               id="product_name"
               name="product_name"
@@ -29,10 +31,12 @@ export default function CreateProduct() {
               value={formState.product_name}
               onChange={handleTextChange}
             />
-          </li>
+          </div>
 
-          <li className="p-2">
-            <label htmlFor="price">Precio:</label>
+          <div className="w-full md:w-1/2">
+            <label htmlFor="price" className="block mb-2">
+              Precio:
+            </label>
             <Input
               type="number"
               id="price"
@@ -41,68 +45,74 @@ export default function CreateProduct() {
               value={formState.price}
               onChange={handleTextChange}
             />
-          </li>
+          </div>
+        </div>
 
-          <li className="p-2">
-            <label htmlFor="description">Descripción:</label>
-            <textarea
-              id="description"
-              name="description"
-              required
-              className="w-full p-2 border rounded"
-              value={formState.description}
-              onChange={handleTextChange}
-            ></textarea>
-          </li>
+        <div className="w-full">
+          <label htmlFor="description" className="block mb-2">
+            Descripción:
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            required
+            className="w-full p-3 border rounded"
+            value={formState.description}
+            onChange={handleTextChange}
+          ></textarea>
+        </div>
 
-          <li className="p-2">
-            <label htmlFor="category_id">Categoría:</label>
-            <select
-              id="category_id"
-              name="category_id"
-              required
-              className="w-full p-2 border rounded"
-              value={formState.category_id}
-              onChange={handleTextChange}
-            >
-              <option value="">Selecciona una categoría</option>
-              <option value="1">Programming & Tech</option>
-              <option value="2">Graphics & Design</option>
-              <option value="3">Marketing</option>
-              <option value="4">Copywriting</option>
-              <option value="5">AI Services</option>
-              <option value="6">Finance</option>
-              <option value="7">Business</option>
-            </select>
-          </li>
+        <div className="w-full">
+          <label htmlFor="category_id" className="block mb-2">
+            Categoría:
+          </label>
+          <select
+            id="category_id"
+            name="category_id"
+            required
+            className="w-full p-3 border rounded"
+            value={formState.category_id}
+            onChange={handleTextChange}
+          >
+            <option value="">Selecciona una categoría</option>
+            <option value="1">Programming & Tech</option>
+            <option value="2">Graphics & Design</option>
+            <option value="3">Marketing</option>
+            <option value="4">Copywriting</option>
+            <option value="5">AI Services</option>
+            <option value="6">Finance</option>
+            <option value="7">Business</option>
+          </select>
+        </div>
 
-          <li className="p-2">
-            <input
-              id="photo1"
-              type="file"
-              name="photo1"
-              accept="image/*"
-              onChange={handleFileChange}
-              className="hidden"
-            />
-            <label
-              htmlFor="photo1"
-              className="block bg-amarillo text-white text-center py-2 px-4 rounded cursor-pointer"
-            >
-              Subir foto
-            </label>
-            {formState.photo1 && (
-              <p className="text-sm text-gray-600 mt-2">
-                {formState.photo1.name}
-              </p>
-            )}
-          </li>
-        </ul>
+        <div className="w-full text-center">
+          <input
+            id="photo1"
+            type="file"
+            name="photo1"
+            accept="image/*"
+            onChange={handleFileChange}
+            className="hidden"
+          />
+          <label
+            htmlFor="photo1"
+            className="inline-block bg-amarillo text-white py-2 px-6 rounded cursor-pointer"
+          >
+            Subir foto
+          </label>
+          {formState.photo1 && (
+            <p className="text-sm text-gray-600 mt-2">
+              {formState.photo1.name}
+            </p>
+          )}
+        </div>
 
-        <button className="bg-gris1 hover:bg-amarillo2 text-white font-bold py-3 px-6 rounded-lg text-lg transition-colors mt-4">
-          Crear Producto
+        <button type="submit" className="boton boton-amarillo">
+          Crear producto
         </button>
       </form>
+
+      <ToastContainer position="bottom-right" />
     </main>
   );
 }
