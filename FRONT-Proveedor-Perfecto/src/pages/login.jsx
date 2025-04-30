a903335a67f0019beab1c602960b17e3ac97da29
Patricio Escalona <patricioescabe@gmail.com>
2025-04-30 17:09:22 +0200

    Login depurado

--- FRONT-Proveedor-Perfecto/src/pages/login.jsx
+++ FRONT-Proveedor-Perfecto/src/pages/login.jsx
@@ -10,59 +10,62 @@ export default function Login() {
         Bienvenido de nuevo
       </h3>
 
-    <div className="w-screen h-80 flex flex-col items-center">
-      <h2 className="text-cta md:text-[36px] text-negro font-bold">Login</h2>
-      {error && <p className="error">{error}</p>}
-      <form
-        className=" border-gris1  text-normal text-negro font-bold text-center w-full max-w-xs gap-6 "
-        onSubmit={handleSubmit}
-      >
-        <ul className=" ">
-          <li className="p-2  ">
-            {/* <label className="block text-sm font-bold mb-2" htmlFor="email">
+      <div className="w-screen h-80 flex flex-col items-center">
+        {error && <p className="error">{error}</p>}
+        <form
+          className=" border-gris1  text-normal text-negro font-bold text-center w-full max-w-xs gap-6 "
+          onSubmit={handleSubmit}
+        >
+          <ul className=" ">
+            <li className="p-2  ">
+              {/* <label className="block text-sm font-bold mb-2" htmlFor="email">
               Email:{" "}
             </label> */}
-            <Input
-              className="formulario bg-blanco"
-              placeholder="Email:"
+              <Input
+                className="formulario bg-blanco"
+                placeholder="Email:"
+                type="email"
+                required
+                id="email"
+                name="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+            </li>
 
-              type="email"
-              required
-              id="email"
-              name="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-          </li>
+            <li className="p-2">
+              <label
+                className="block text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                {" "}
+              </label>
+              <Input
+                className="formulario bg-blanco"
+                type="password"
+                required
+                id="password"
+                name="password"
+                placeholder="Contraseña"
+                value={formState.password}
+                onChange={handleChange}
+              />
+            </li>
+            <li>
+              <a
+                href="https://www.google.es"
+                className="text-blue-600 underline"
+              >
+                ¿Has olvidado tu contraseña?
+              </a>
+            </li>
+          </ul>
 
-          <li className="p-2">
-            <label className="block text-sm font-bold mb-2" htmlFor="password">
-              {" "}
-            </label>
-            <Input
-              className="formulario bg-blanco"
-
-              type="password"
-              required
-              id="password"
-              name="password"
-              placeholder="Contraseña"
-              value={formState.password}
-              onChange={handleChange}
-            />
-          </li>
-          <li>
-            <link rel="stylesheet" href="www.google.es" />
-            Haz olvidado tu contraseña?
-          </li>
-        </ul>
-        <button className="boton bg-amarillo cursor-pointer justify-self-center ">
-
-        <button className="boton bg-amarillo cursor-pointer ">
-          Inicia sesión
-
-        </button>
-      </form>
+          <button className="boton bg-amarillo cursor-pointer justify-self-center  ">
+            Inicia sesión
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
