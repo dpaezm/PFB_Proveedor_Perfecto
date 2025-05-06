b1bee85d69e2bb18b18366ac1e8f6f7c6d1d6967
dpaezm <diegopaezmacias@gmail.com>
2025-05-06 22:10:40 +0200

    Recuperar trabajo del stash tras cherry-pick

--- FRONT-Proveedor-Perfecto/src/pages/UserData.jsx
+++ FRONT-Proveedor-Perfecto/src/pages/UserData.jsx
@@ -2,6 +2,7 @@ import "./userData.css";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import useUserProfile from "../hooks/UseUserProfile";
+import ProviderFull from "../components/User/ProviderFull";
 const { VITE_API_URL, VITE_API_MEDIA_PATH } = import.meta.env;
 
 export default function UserData() {
@@ -15,19 +16,20 @@ export default function UserData() {
   if (token)
     return (
       <div className="container-user-data">
-        <section className="container-user-data">
-          <h1 className="titulo-cat2 text-logo md:text-[32px] text-negro"> Mi perfil</h1>
-          <h2>{user.username}</h2>
+        <h2 className="text-logo md:text-[32px] text-negro"> Mi perfil</h2>
+        <ProviderFull provider={user} />
+        {/* <section className="container-user-data">
+          <h3>{user.username}</h3>
           <p>{user.name}</p>
 
-          <img src={`${VITE_API_MEDIA_PATH}${user.avatar}`} alt={user.username} />
+          <img src={`${VITE_API_URL}/${user.avatar}`} alt={user.username} />
 
           <p>Ciudad: {user.city}</p>
           <p>Contacto: {user.email}</p>
           <p>Teléfono: {user.phone}</p>
           <p>Desde: {user.created_at}</p>
           <p>Sobre mi: {user.description}</p>
-        </section>
+        </section> */}
         <section className="section-buttons-user-data gap-2">
           {isProvider ? (
             <button onClick={() => navigate(`/contactrequest/${user.id}`)} className="boton boton2 boton-amarillo2">
